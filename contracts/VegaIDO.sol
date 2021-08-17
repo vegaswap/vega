193b7026a4e0c83da3c4bb1ca65e23896f68faba
benjyz <ben@vegaswap.io>
2021-08-17 20:11:32 +0700

    ido and nrt

--- contracts/VegaIDO.sol
+++ contracts/VegaIDO.sol
@@ -3,59 +3,84 @@ pragma solidity ^0.8.5;
 
 import "./IERC20.sol";
 import "./Ownable.sol";
+// import "./Util.sol";
+import "./NRT.sol";
 
 // Initial Dex Offering (IDO) contract for launching ventures
 // assumes that decimals are 18 for vega and investToken
-contract VegaIDO is Ownable {
-    address public vegaTokenAddress;
-    address public investTokenAddress;
+// only tracks invest amount
+// state 1: 50$ x 1000 tickets
+// state 2: 800 are filled (200 left, 200x 50$)
+// state 3: set cap to 200$ (5x guys invest more)
+// state 4: FCFS
 
+//TODO: NRT
+contract VegaIDO is Ownable {
+    
+    address public investTokenAddress;
     IERC20 public investToken;
-    IERC20 public vegaToken;
+    NRT public nrt;
 
     // single round
-    // price tokens are sold at
-    uint256 public askPriceMultiple;
     // total maximum amount invested
-    uint256 public cap;
+    uint256 public totalcap;
+    uint256 public askpriceMultiple;
     // total amount invested
-    uint256 invested;
-    address[] whitelistAddresses;
-    mapping(address => uint256) whitelistAmounts;
+    uint256 public totalInvested;
+    address[] public whitelistAddresses;
+    uint256 public capPerAccount;
+    uint256 startTime;
+    uint256 endTime;
+
+
+    //mapping(address => uint256) whitelistAmounts;
 
     // investors and how much they invest
-    mapping(address => uint256) investors;
+    mapping(address => uint256) investedAmounts;
 
-    event Invested(address, uint256);
+    event InvestEvent(address, uint256);
 
-    constructor(
-        address _launchTokenAddress,
-        address _investTokenAddress,
-        uint256 _askPriceMultiple,
-        uint256 _cap
-    ) {
-        vegaTokenAddress = _launchTokenAddress;
+    constructor(address _investTokenAddress, uint256 _askpriceMultiple, uint256 _totalcap, uint256 _capPerAccount, uint256 _startTime, uint256 _endTime) {
         investTokenAddress = _investTokenAddress;
 
         investToken = IERC20(investTokenAddress);
-        vegaToken = IERC20(vegaTokenAddress);
-        askPriceMultiple = _askPriceMultiple;
-        cap = _cap;
+        totalcap = _totalcap;
+        capPerAccount = _capPerAccount;
+        askpriceMultiple = _askpriceMultiple;
+        startTime = _startTime;
+        endTime = _endTime;
+        // nrt = new NRT("VegaNRT");
     }
 
-    function addWhiteList(
-        address[] memory addresses,
-        uint256[] memory maxInvestAmounts
-    ) external onlyOwner {
-        require(
-            addresses.length == maxInvestAmounts.length,
-            "Addresses and amounts array lengths must match"
-        );
+    // function string memory reason)
+    //     private
+    //     pure
+    //     returns (string memory)
+    // {
+    //     return Util."VegaIDO", reason);
+    // }
 
+    // function string memory reason, string memory data)
+    //     private
+    //     pure
+    //     returns (string memory)
+    // {
+    //     return Util."VegaIDO", reason, data);
+    // }
+
+    //in case not enough filled
+    function setCapPerAccount (uint256 _capPerAccount) public onlyOwner {
+        require(_capPerAccount > capPerAccount,"should be bigger than before");
+        capPerAccount = _capPerAccount;
+    }
+
+    // collected from external
+    function addWhiteList(
+        address[] memory addresses
+    ) external onlyOwner {
         for (uint256 i = 0; i < addresses.length; i++) {
             address w = addresses[i];
             whitelistAddresses.push(w);
-            whitelistAmounts[w] = maxInvestAmounts[i];
         }
     }
 
@@ -69,68 +94,47 @@ contract VegaIDO is Ownable {
         return false;
     }
 
-    function wlAmount(address a) public view returns (uint256) {
-        return whitelistAmounts[a];
-    }
-
     // participate in the funding event
     function invest(uint256 investAmount) external {
-        require(inWhitelist(msg.sender), "VegaIDO: not whitelisted");
+        require(block.timestamp > startTime, "IDO not started");
+        require(block.timestamp < endTime, "IDO has ended");
+        require(inWhitelist(msg.sender), "not whitelisted");
         require(
-            invested + investAmount <= cap,
-            "VegaIDO: Cap for current round reached"
+            totalInvested + investAmount <= totalcap,
+                "totalcap for current round reached"
+                // Util.uintToString(totalInvested + investAmount
         );
-        //TODO
-        require(
-            investAmount != whitelistAmounts[msg.sender],
-            "VegaIDO: Invest amount is not equal to the whitelist amount"
-        );
+
+        //will be zero if first time investor
+        uint256 investedAmount = investedAmounts[msg.sender];
+        
+        require(investedAmount + investAmount <= capPerAccount, "more than cap invested");
+        
         require(
             investToken.allowance(msg.sender, address(this)) >= investAmount,
-            "VegaIDO: Please approve amount to invest"
+            "Please approve amount to invest"
         );
         require(
             investToken.balanceOf(msg.sender) >= investAmount,
-            "VegaIDO: Insufficient balance to invest"
+            "Insufficient balance to invest"
         );
-        require(investors[msg.sender] == 0, "VegaIDO: Already invested");
-
-        //price takes into account decimals
-        uint256 buyTokenAmount = investAmount * askPriceMultiple;
-        require(
-            vegaToken.balanceOf(address(this)) >= buyTokenAmount,
-            "VegaIDO: out of stock"
-        );
-
+        
         // Transfer tokens from sender to this contract
         bool ts = investToken.transferFrom(
             msg.sender,
             address(this),
             investAmount
         );
-        require(ts, "VegaIDO: transfer invest tokens failed");
+        require(ts, "transfer invest tokens failed");
 
-        bool vts = vegaToken.transfer(msg.sender, buyTokenAmount);
-        require(vts, "VegaIDO: transfer vega tokens failed");
+        investedAmounts[msg.sender] += investAmount;
+        totalInvested += investAmount;
 
-        investors[msg.sender] += investAmount;
-        invested += investAmount;
+        uint256 nrtReceived = investAmount * askpriceMultiple;
+        //TODO check decimals
+        nrt.issue(msg.sender, nrtReceived);
 
-        emit Invested(msg.sender, investAmount);
-    }
-
-    function depositTokens(uint256 amount) public onlyOwner {
-        require(
-            vegaToken.allowance(owner(), address(this)) >= amount,
-            "VegaIDO: deposit failed"
-        );
-        vegaToken.transferFrom(owner(), address(this), amount);
-    }
-
-    // withdraw any left over tokens
-    function withDrawTokens(uint256 amount) public onlyOwner {
-        bool b = vegaToken.transfer(owner(), amount);
-        require(b, "VegaIDO: withdraw failed");
+        emit InvestEvent(msg.sender, investAmount);
     }
 
     // withdraw the funding amount
