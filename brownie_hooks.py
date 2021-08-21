6d6029380602bab24448b5a527ac7d42247c321d
benjyz <ben@vegaswap.io>
2021-08-21 11:10:43 +0700

    clean

--- brownie_hooks.py
+++ brownie_hooks.py
@@ -9,16 +9,18 @@ from brownie import accounts
 from web3 import Web3
 import json
 import os
-from stat import S_IREAD, S_IRGRP, S_IROTH, S_IWUSR 
+from stat import S_IREAD, S_IRGRP, S_IROTH, S_IWUSR
+import hashlib
 
-contractout = ''
+contractout = ""
 global solcount, vycount
-solcount, vycount = 0,0
+solcount, vycount = 0, 0
 
 deployedlibs = {}
 
+
 def pre():
-    """ set the out directory once at setup """
+    """set the out directory once at setup"""
     global contractout
     with open("./brownie-config.yaml", "r") as f:
         z = f.read()
@@ -26,16 +28,18 @@ def pre():
     config = yaml.safe_load(z)
     contractout = config["project_structure"]["contracts-post"]
 
+
 def get_contract(w3, ctr):
-    with open("./build/contracts/%s.json"%ctr,"r") as f:
+    with open("./build/contracts/%s.json" % ctr, "r") as f:
         j = json.loads(f.read())
         contract = w3.eth.contract(abi=j["abi"], bytecode=j["bytecode"])
         return contract
 
+
 def deploy_lib(contractName):
     accounts.load("vegabsctest")
     mainaccount = accounts[0]
-   
+
     URL = "http://localhost:8545"
     w3 = Web3(Web3.HTTPProvider(URL))
     ctr = get_contract(w3, contractName)
@@ -45,11 +49,12 @@ def deploy_lib(contractName):
     # estgas = w3.eth.estimate_gas({'from': mainaccount.address})
 
     txparams = {
-                'from': mainaccount.address,
-                # 'to': '',
-                "nonce": nonce,
-                "gas": 660000,
-                "gasPrice": w3.toWei('5', 'gwei')}
+        "from": mainaccount.address,
+        # 'to': '',
+        "nonce": nonce,
+        "gas": 660000,
+        "gasPrice": w3.toWei("5", "gwei"),
+    }
 
     # txparams["gas"] = estgas
 
@@ -63,11 +68,12 @@ def deploy_lib(contractName):
     # print(tx_receipt['status'])
     # print(tx_receipt['blockNumber'])
     deployedAddress = tx_receipt.contractAddress
-    print("gasUsed ",tx_receipt['gasUsed'])
+    print("gasUsed ", tx_receipt["gasUsed"])
     return deployedAddress
 
+
 def pre_hook():
-    print (">>>>> START <<<<<<")
+    print(">>>>> START <<<<<<")
     pre()
     print("Name\tType")
     # deployedAddress = deploy_lib("Util")
@@ -79,50 +85,45 @@ def pre_hook():
 
     # print (w3.eth.getBalance(mainaccount.address))
 
+
 def post_hook():
-    print (">>>>> DONE <<<<<<")
-    print ("processed %i solidity contracts"%solcount)
-    print ("processed %i vyper contracts"%vycount)
-    
+    print(">>>>> DONE <<<<<<")
+    print("processed %i solidity contracts" % solcount)
+    print("processed %i vyper contracts" % vycount)
+
 
 def contract_type(lines):
     for line in lines:
-        if line == "": continue
-        if line.startswith("//"): continue
-        if line.startswith("import"): continue
-        if line.startswith("pragma"): continue
+        if line == "":
+            continue
+        if line.startswith("//"):
+            continue
+        if line.startswith("import"):
+            continue
+        if line.startswith("pragma"):
+            continue
 
-        if line.startswith("contract"): return "contract"
-        if line.startswith("abstract contract"): return "abstractcontract"
-        if line.startswith("library"): return "library"
-        if line.startswith("interface"): return "interface"
+        if line.startswith("contract"):
+            return "contract"
+        if line.startswith("abstract contract"):
+            return "abstractcontract"
+        if line.startswith("library"):
+            return "library"
+        if line.startswith("interface"):
+            return "interface"
 
     return "Error"
 
-def iscontract(lines):
-    #find the definition line
-    pass
-    
-
-    # for line in lines:
-    #     if line.startsWith("contract"):
-    #         return True
-    #     if line.startsWith("abstract contract"):
-    #         return False
-    #     if line.startWith("library"):
-    #         return False
-    #     if line.startsWith("interface"):
-    #         return False
 
 def replace_template(source, contractName):
-# TODO
+    # TODO
     # just insert contract name after contract line instead
     t = Template(source)
 
     # util_address = "0x0c11645843Ea5edC413efB304692d8Ccb06ED4D2"
-    #TESTNET
+    # TESTNET
     util_address = deployedlibs["Util"]
-    print (">>>>>>> ", util_address)
+    print(">>>>>>> ", util_address)
     # util_address = "0xa3B53dDCd2E3fC28e8E130288F2aBD8d5EE37472"
 
     debugfunctions = """IUtil constant util =
@@ -162,26 +163,28 @@ def replace_template(source, contractName):
     source = t.render(
         # header='string public constant contractName = "%s";' % contractName,
         debugfunctions=debugfunctions
-    )    
+    )
     return source
+
 
 def get_content(fp):
     with open(fp, "r") as f:
         return f.read()
 
+
 def fetch_source_for_line(line):
-    #assume import statement like this
-    #import "./MaxSupplyToken.sol";
-    print("deal with ",line)
+    # assume import statement like this
+    # import "./MaxSupplyToken.sol";
+    print("deal with ", line)
     ctr = line.split('"')[1:-1]
     ctr = "".join(ctr)
     ctr = ctr[2:]
-    print (ctr)
-    fp = "./contracts-pre/%s"%ctr
+    print(ctr)
+    fp = "./contracts-pre/%s" % ctr
     z = get_content(fp)
 
     newlines = list()
-    addedlines = z.split('\n')
+    addedlines = z.split("\n")
     for line in addedlines:
         # // SPDX-License-Identifier: MIT
         # pragma solidity ^0.8.5;
@@ -192,13 +195,15 @@ def fetch_source_for_line(line):
 
     return newlines
 
+
 def count_import(lines):
     i = 0
     for line in lines:
         if "import" in line:
-            i+=1
-            
+            i += 1
+
     return i
+
 
 def replace_import(lines):
     newlines = list()
@@ -209,100 +214,85 @@ def replace_import(lines):
                 newlines.append(line)
             # print (z)
         else:
-            newlines.append(line)    
+            newlines.append(line)
     return newlines
+
 
 def remove_empty_lines(lines):
     newlines = list()
-    # ins = False
     stop = False
     for line in lines:
         if "interface" in line or "contract" in line:
             stop = True
         if not stop:
-            print(line)
-            isempty = (line == "")
+            # print(line)
+            isempty = line == ""
             if not isempty:
                 newlines.append(line)
             else:
-                print ("skip ", line)
+                print("skip ", line)
         else:
             newlines.append(line)
     return newlines
 
 
 def write_lines(fp, lines):
-    print ("write to ",fp, len(lines))
+    print("write to ", fp, len(lines))
     lines = remove_empty_lines(lines)
     newsrc = "\n".join(lines)
     with open(fp, "w") as f:
         f.write(newsrc)
 
+
 def gen_flatfile(contractName, source):
-    lines = source.split('\n')
-    
+    lines = source.split("\n")
+
     newlines = replace_import(lines)
-    print ("!! imports ", count_import(newlines))
-    print (" len: ", len(newlines))
+    print("imports ", count_import(newlines))
+    print("len: ", len(newlines))
 
+    # run second time, recursion would be nicer
     newlines = replace_import(newlines)
-    print ("!! imports ", count_import(newlines))
-    print (" len: ", len(newlines))
+    print("imports ", count_import(newlines))
+    print(" len: ", len(newlines))
 
-    
     # print("newsrc ",len(newsrc))
 
-    fp = "./%s/%s.sol"%(contractout,contractName)
+    fp = "./%s/%s.sol" % (contractout, contractName)
     write_lines(fp, newlines)
-    
 
-#     #run through second time
-#     # zlines = list()
-#     # for line in newlines:
-#     #     if "import" in line:
-#     #         added = fetch_source_for_line(line)
-#     #         for line in added:
-#     #             zlines.append(added)
-#     #     print (type(line), line)
-#     #     zlines.append(line)
-
-#     # newsrc = "\n".join(zlines)
-    
-import hashlib
 
 def sha1sum(filename):
-    # h  = hashlib.sha256()
-    h  = hashlib.sha1()
-    b  = bytearray(128*1024)
+    h = hashlib.sha1()
+    b = bytearray(128 * 1024)
     mv = memoryview(b)
-    with open(filename, 'rb', buffering=0) as f:
-        for n in iter(lambda : f.readinto(mv), 0):
+    with open(filename, "rb", buffering=0) as f:
+        for n in iter(lambda: f.readinto(mv), 0):
             h.update(mv[:n])
     return h.hexdigest()
+
 
 def process_source(path, source):
     global solcount, vycount
     contractName = str(path.name).split(".")[0]
     contractEnd = str(path.name).split(".")[1]
-    lines = source.split('\n')
+    lines = source.split("\n")
     # print ("lines ", len(lines))
     ctrtype = contract_type(lines)
-    print("%s\t%s"%(contractName,ctrtype))
+    print("%s\t%s" % (contractName, ctrtype))
     # print("type ", contractEnd)
     if contractEnd == "sol":
-        solcount+=1
+        solcount += 1
     if contractEnd == "vy":
-        vycount+=1
+        vycount += 1
 
     # print (solcount, vycount)
 
-    # source = replace_template(source, contractName)    
+    # source = replace_template(source, contractName)
 
-    
     fn = "%s/%s" % (contractout, path.name)
     # filename = "path/to/file"
     # print(fn)
-    
 
     # os.chmod(fn, S_IWUSR|S_IREAD)
     # with open(fn, "w") as f:
@@ -311,25 +301,21 @@ def process_source(path, source):
     # read only file
     # os.chmod(fn, S_IREAD|S_IRGRP|S_IROTH)
 
-
-    # TODO generate flat file
-    i = 0
     if contractName == "VegaToken":
         gen_flatfile(contractName, source)
-        
-    
+
     hashsol = sha1sum("./contracts-post/VegaToken.sol")
     hashabi = sha1sum("./contracts-post/VegaToken.abi")
     hashbin = sha1sum("./contracts-post/VegaToken.bin")
 
-    with open("./contracts-post/contracts.md","w") as f:
+    with open("./contracts-post/contracts.md", "w") as f:
         # Vega contracts
         f.write("# Vega contracts\n\n")
         f.write("| File | sha1|\n")
         f.write("| :--: | :--:|\n")
-        f.write("| VegaToken.sol | %s |\n"%hashsol)
-        f.write("| VegaToken.abi | %s |\n"%hashabi)
-        f.write("| VegaToken.bin | %s |\n"%hashbin)
+        f.write("| VegaToken.sol | %s |\n" % hashsol)
+        f.write("| VegaToken.abi | %s |\n" % hashabi)
+        f.write("| VegaToken.bin | %s |\n" % hashbin)
 
     return source
 
--- build_process.py
+++ build_process.py
@@ -1,19 +1,22 @@
 import json
 
+
 def getj(ctr):
-    fp = "./build/contracts/%s.json"%ctr
-    with open(fp,"r") as f:
+    fp = "./build/contracts/%s.json" % ctr
+    with open(fp, "r") as f:
         return json.loads(f.read())
+
 
 import hashlib
 
+
 def sha256sum(filename):
     # h  = hashlib.sha256()
-    h  = hashlib.sha1()
-    b  = bytearray(128*1024)
+    h = hashlib.sha1()
+    b = bytearray(128 * 1024)
     mv = memoryview(b)
-    with open(filename, 'rb', buffering=0) as f:
-        for n in iter(lambda : f.readinto(mv), 0):
+    with open(filename, "rb", buffering=0) as f:
+        for n in iter(lambda: f.readinto(mv), 0):
             h.update(mv[:n])
     return h.hexdigest()
 
@@ -26,7 +29,5 @@ def sha256sum(filename):
 
 
 ctr = "NRT"
-shasum = sha256sum("./contracts/%s.bin"%ctr)
-print (shasum)
-
-
+shasum = sha256sum("./contracts/%s.bin" % ctr)
+print(shasum)
