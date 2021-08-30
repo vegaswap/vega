761bd1de2d737157c6d0377ffaab12f208704408
benjyz <ben@vegaswap.io>
2021-08-30 14:15:16 +0700

    more testing

--- compile.sh
+++ compile.sh
@@ -2,11 +2,9 @@
 sudo rm build/*.*
 cd contracts-vy
 
-vyper Bucket.vy -f abi > ../build/Bucket.abi
-vyper Bucket.vy -f bytecode > ../build/Bucket.bin
-
-vyper VegaToken.vy -f abi > ../build/VegaToken.abi
-vyper VegaToken.vy -f bytecode > ../build/VegaToken.bin
-
-vyper Xlist.vy -f abi > ../build/Xlist.abi
-vyper Xlist.vy -f bytecode > ../build/Xlist.bin
+ vyper -o ../build Bucket.vy -f abi
+ vyper -o ../build Bucket.vy -f bytecode
+ vyper -o ../build VegaToken.vy -f abi
+ vyper -o ../build VegaToken.vy -f bytecode
+ vyper -o ../build Xlist.vy -f abi
+ vyper -o ../build Xlist.vy -f bytecode
--- tests/test_approve.py
+++ tests/test_approve.py
@@ -9,18 +9,25 @@ from setup_tests import *
 # def test_initial_approval_is_zero(token, test_accounts, idx):
 #     assert token.allowance(accounts[0], accounts[idx]) == 0
 
-def test_approve(token, accounts):
+def test_approve(token, accounts, transactor):
     assert accounts
-    token.approve(accounts[1], 10 ** 19, {"from": accounts[0]})
-    # assert token.allowance(accounts[0], accounts[1]) == 10 ** 19
+    f = token.f.approve(accounts[1].address, 10 ** 19)
+    transactor.buildpush(f, accounts[0])
+    assert token.f.allowance(accounts[0].address, accounts[1].address).call() == 10 ** 19
 
 
+def test_modify_approve(token, accounts, transactor):
+    f = token.f.approve(accounts[1].address, 10 ** 19) 
+    transactor.buildpush(f, accounts[0])
+    assert token.f.allowance(accounts[0].address, accounts[1].address).call() == 10 ** 19
 
-# def test_modify_approve(token, accounts):
-#     token.approve(accounts[1], 10 ** 19, {"from": accounts[0]})
-#     token.approve(accounts[1], 12345678, {"from": accounts[0]})
+    f = token.f.approve(accounts[1].address, 0) 
+    transactor.buildpush(f, accounts[0])
+    assert token.f.allowance(accounts[0].address, accounts[1].address).call() == 0
 
-#     assert token.allowance(accounts[0], accounts[1]) == 12345678
+    f = token.f.approve(accounts[1].address, 5) 
+    transactor.buildpush(f, accounts[0])
+    assert token.f.allowance(accounts[0].address, accounts[1].address).call() == 5
 
 
 # def test_revoke_approve(token, accounts):
