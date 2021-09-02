98c03f162b4700f950beb226ca922f5fb9ccc290
benjyz <ben@vegaswap.io>
2021-09-02 16:08:37 +0700

    odd data fails

--- tests/test_realdata.py
+++ tests/test_realdata.py
@@ -78,7 +78,9 @@ def test_claim_list_many(token, accounts):
 
     assert bucket.openClaimAmount() == 900
 
-    # for x in range(0,nump):
+    assert bucket.duration() ==10*default_period
+    assert bucket.endTime() == bucket.cliffTime() + 10*default_period
+
     for x in range(0,nump):
         chain.sleep(days)
         bucket.vestAll()
@@ -90,10 +92,54 @@ def test_claim_list_many(token, accounts):
             tb += b
         assert tb == (x+1)*90
 
-    #     tb += x
+    assert bucket.openClaimAmount() == 0
 
-    # # -- 900 vested
-    # assert tb == 1000
-    # # -- 100 left
-    # assert token.balanceOf(bucket) == 0
+    assert token.balanceOf(bucket) == 100
+
+
+def test_claim_list_odd(token, accounts):
+    t = chain.time()
+    cliff = t + 1
+    nump = 11
+    total = 1050
+    days = 86400
+    default_period = 30 * days
+    p = default_period
+    bucket = Bucket.deploy(
+        "Somebucket", token.address, cliff, nump, total, p, {"from": accounts[0]}
+    )
+    bucket.initialize()
+    
+    # -- deposit 1000
+    token.approve(bucket, 1000, {"from": accounts[0]})
+    bucket.depositOwner(1000)
+    assert bucket.openClaimAmount() == 0
+
+    for i in range(1, 10):
+        bucket.addClaim(accounts[i], 100)
+
+    assert bucket.openClaimAmount() == 900
+
+    amountPerPeriod = int(total / nump)
+    print(amountPerPeriod)
+    assert amountPerPeriod == int(1050/11)
+    from vmath import ceildiv
+    duration = p * ceildiv(total, amountPerPeriod)
+    assert bucket.duration() ==duration
+    assert bucket.endTime() == bucket.cliffTime() + bucket.duration()
+
+    for x in range(0,nump+1):
+        chain.sleep(days)
+        bucket.vestAll()
+        # assert bucket.openClaimAmount() == 819 - (x+1)*90
+        tb = 0
+        for i in range(1, 10):
+            b = token.balanceOf(accounts[i])
+            assert b == (x+1)*9
+            tb += b
+        assert tb == (x+1)*81
+
+    assert bucket.openClaimAmount() == 9
+
+    assert token.balanceOf(bucket) == 100
 
